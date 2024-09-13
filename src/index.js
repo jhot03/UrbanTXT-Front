8f459b0061053ff67358704bbf5d9f41b44b836f
Jayce Hottenroth <hottenro@usc.edu>
2024-09-12 23:49:01 -0700

    ready

--- src/index.js
+++ src/index.js
@@ -2,9 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { disableReactDevTool } from '@fvilers/disable-react-devtools'
 
-if (process.env.NODE_ENV === 'production') disableReactDevTool()
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
